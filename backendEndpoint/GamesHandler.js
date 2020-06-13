013e343afdd9f55d3dc35e8918978a05467f8885
Teodor DEACONU <tdeaconu@bbu.bitdefender.biz>
2020-06-13 12:56:35 +0300

    games are launching

--- backendEndpoint/GamesHandler.js
+++ backendEndpoint/GamesHandler.js
@@ -1,11 +1,13 @@
 "use strict";
 const fs = require('fs');
+const exec = require('child_process').execFile;
 const privateKeys = require('./config/privateKeys');
 
 
 class GamesHandler {
 
   allGames;
+  installedGames = [];
 
   constructor(){}
 
@@ -50,6 +52,7 @@ class GamesHandler {
         const response = await fetch(`https://store.steampowered.com/api/appdetails?appids=${appId}`);
         const json = await response.json();
         appIdToGameDetail[appId] = json[appId].data;
+        appIdToGameDetail[appId].playtime = game.playtime_forever;
       }
 
       // write object to file
@@ -59,6 +62,57 @@ class GamesHandler {
     }
 
     return this.allGames;
+  }
+
+  launchGame(gameName) {
+    const parsedName = gameName.replace(/:/g, '');
+    const installedGamesDefaultDirectory = 'D:\\NonWork\\SteamGames\\steamapps\\common';
+    const gameToLaunchPath = `${installedGamesDefaultDirectory}\\${parsedName}`;
+
+    if (fs.existsSync(gameToLaunchPath)) {
+
+      // read files inside game directory
+      const dirs = fs.readdirSync(gameToLaunchPath);
+
+      // filter the .exe files
+      const exeFiles = dirs.filter( (file) => file.includes('.exe'));
+
+      // launch game
+      if (exeFiles.length > 1) {
+
+        console.log(`launching game..`);
+        console.log(`${gameToLaunchPath}\\${exeFiles[exeFiles.length - 1]}`);
+        exec(`${gameToLaunchPath}\\${exeFiles[exeFiles.length - 1]}`);
+
+      } else {
+
+        console.log(`launching game..`);
+        console.log(`${gameToLaunchPath}\\${exeFiles[0]}`);
+        exec(`${gameToLaunchPath}\\${exeFiles[0]}`);
+
+      }
+    } else {
+      console.log('invalid path');
+    }
+  }
+
+  getInstalledGames() {
+    // path of steam games
+    const steamGamesPath = 'D:\\NonWork\\SteamGames\\steamapps\\common\\';
+
+    // check installed games
+    const games = fs.readdirSync(steamGamesPath);
+
+    // from all available games, take only the ones found in pc
+    for (const appid in this.allGames) {
+      if(this.allGames.hasOwnProperty(appid)) {
+        if (games.includes(this.allGames[appid].name.replace(/:/g, ''))) {
+          this.installedGames.push(this.allGames[appid]);
+        }
+      }
+    }
+
+    return this.installedGames;
   }
 
 }
